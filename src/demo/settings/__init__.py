e6214a7e52b9ec9bb912813295406b0a8524bf2e
pollitosabroson <karnal02@gmail.com>
2015-05-28 10:03:44 -0500

    config database

--- src/demo/settings/__init__.py
+++ src/demo/settings/__init__.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,8 +57,8 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'demo',
     }
 }
 
